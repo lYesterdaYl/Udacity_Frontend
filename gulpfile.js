e0dbab9e77821da31203afbf769fce088e741468
lYesterdaYl <bgmds@126.com>
2018-10-01 22:55:39 -0700

    fix
    removed gulp. I will use gulp for a specific project in the future.

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const autoprefixer = require("gulp-autoprefixer");
-
-gulp.task("default", function() {
-    console.log("hello world!");
-});
